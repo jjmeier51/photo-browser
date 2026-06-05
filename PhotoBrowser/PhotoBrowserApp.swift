65efdccb31ea9b62f3fba207a489c56e3fbd779e
John Meier <john@opsgenie.com>
2026-06-05 12:36:42 -0400

    Initial Commit

---
+++ PhotoBrowser/PhotoBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoBrowserApp.swift
+//  PhotoBrowser
+//
+//  Created by John Meier on 6/5/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
